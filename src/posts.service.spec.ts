ddf6a63c54ada1e1dd36ddb256320cdf4c910d40
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2025-04-28 11:40:45 +0300

    Update posts.service.spec.ts

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,10 +1,10 @@
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
-  let postsService: PostsService;
+  let service: PostsService;
 
   beforeEach(() => {
-    postsService = new PostsService();
+    service = new PostsService();
   });
 
   describe('.findMany', () => {
@@ -16,17 +16,48 @@ describe('PostsService', () => {
     ];
 
     beforeEach(() => {
-      posts.forEach((post) => postsService.create(post));
+      posts.forEach((post) => service.create(post));
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const received = service.findMany();
+      expect(received.length).toEqual(4);  
+      expect(received[0].text).toEqual(posts[0].text);  
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const received = service.findMany({skip: 1, limit: 1});
+      expect(received.length).toEqual(1);  
+      expect(received[0].text).toEqual(posts[1].text);  
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('Проверка метода find', () => {
+      const received = service.find('1');
+      expect(received?.id).toEqual('1');
+      expect(received?.text).toEqual(posts[0].text);  
+    });
+
+    it('Проверка метода delete', () => {
+      service.delete('1');
+      let received = service.find('1');
+      expect(received).toBeUndefined();  
+      let received2 = service.findMany();
+      expect(received2.length).toEqual(3);  
+    });
+
+    it('Проверка метода update - корректная', () => {
+      const textUpdate = 'Post Update'
+      service.update('1', {text: textUpdate});
+      let received = service.find('1');
+      expect(received?.id).toEqual('1');
+      expect(received?.text).toEqual(textUpdate);  
+    });
+
+    it('Проверка метода update - не валидный ID', () => {
+        const error = () => service.update('ывыв', {text: 'Post Update'});
+        expect(() => error()).toThrow(Error)        
+        expect(() => error()).toThrowError('Пост не найден')        
+    });
+
   });
 });
